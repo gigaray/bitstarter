71d867c95690324e21ca30c6f47c6fce7047d037
John Doe <example@stanford.edu>
2013-07-21 07:14:16 +0000

    ... rewrite ...

--- web.js
+++ web.js
@@ -4,26 +4,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf;
+var buf = new Buffer();
 
 app.get('/', function(request, response) {
 
-   fs.readFile('./index.html', function(err,data) {
+   response.send(readFileSync("./index.html").toString());
 
-	if (err) {
-		throw err;
-	}
-
-	buf = data;
-
-     	console.log(buf);
   });
 
-  response.send(buf.toString());
-
-});
-
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
+
   console.log("Listening on " + port);
+
 });
