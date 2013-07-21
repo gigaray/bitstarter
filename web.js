776f616b6440dc33b1c3f434beeb579d431b4623
John Doe <example@stanford.edu>
2013-07-21 07:29:51 +0000

    retesting ..

--- web.js
+++ web.js
@@ -2,28 +2,22 @@ var fs = require('fs');
 
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express(); 
 
-var buf;
+app.use(express.logger());
+
+var buf = new Buffer();
 
 app.get('/', function(request, response) {
 
-   fs.readFile('./index.html', function(err,data) {
+   response.send(readFileSync("./index.html").toString());
 
-	if (err) {
-		throw err;
-	}
-
-	buf = data;
-
-     	console.log(buf);
   });
 
-  response.send(buf.toString());
-
-});
-
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
+
   console.log("Listening on " + port);
+
 });
